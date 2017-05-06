70ce47c37f5d511142ca32a97f59c7f98696090e
Barendregt <barendregt@fastmail.com>
2017-05-06 21:45:20 +0200

    update

--- PupilAnalyzer.py
+++ PupilAnalyzer.py
@@ -591,9 +591,9 @@ class PupilAnalyzer(Analyzer):
 		self.fir_betas = sp.linalg.lstsq(self.design_matrix.T, sp.signal.resample(recorded_pupil_signal, int(recorded_pupil_signal.shape[-1] / self.signal_sample_frequency*self.deconv_sample_frequency), axis = -1).T)[0]
 
 		
-		embed()
-		pe_betas = self.fir_betas[-events.shape[0]*(resp_deconv_interval[1]-resp_deconv_interval[0])*self.deconv_sample_frequency:].reshape((events.shape[0],(resp_deconv_interval[1]-resp_deconv_interval[0])*self.deconv_sample_frequency)).T
-		other_betas = self.fir_betas[:-events.shape[0]*(nuiss_deconv_interval[1]-nuiss_deconv_interval[0])*self.deconv_sample_frequency].reshape((events.shape[0],(nuiss_deconv_interval[1]-nuiss_deconv_interval[0])*self.deconv_sample_frequency)).T
+		# embed()
+		pe_betas = self.fir_betas[-int(events.shape[0]*(resp_deconv_interval[1]-resp_deconv_interval[0])*self.deconv_sample_frequency):].reshape((events.shape[0],(resp_deconv_interval[1]-resp_deconv_interval[0])*self.deconv_sample_frequency)).T
+		other_betas = self.fir_betas[:-int(events.shape[0]*(resp_deconv_interval[1]-resp_deconv_interval[0])*self.deconv_sample_frequency)].reshape((events.shape[0],(nuiss_deconv_interval[1]-nuiss_deconv_interval[0])*self.deconv_sample_frequency)).T
 
 		return [[pe_betas, other_betas], [self.FIR2.covariates.keys(), self.FIR1.covariates.keys()]]
 
