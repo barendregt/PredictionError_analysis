91a640f8bd3659fc8d28aba12164930c39311b06
Barendregt <barendregt@fastmail.com>
2017-05-11 17:35:46 +0200

    update

--- GLM_analysis.py
+++ GLM_analysis.py
@@ -161,7 +161,7 @@ for subname in sublist:
 		linear_model.configure(IRF='pupil', IRF_params={'dur':3, 's':1.0/(10**26), 'n':10.1, 'tmax':0.93}, regressor_types=['stick','box','stick'])
 		linear_model.execute()
 
-		bvals = [max([b,0]) for b in linear_model.betas]
+		bvals = [np.max([b,0]) for b in linear_model.betas]
 		all_betas[['PP','UP','PU','UU'][tcii]].append(linear_model.betas)
 	# except:
 	# 	embed()
