368e6ab98c654fb900118cfa238bccd07eb2450b
Barendregt <barendregt@fastmail.com>
2017-06-30 23:19:19 +0200

    update

--- analysis_parameters.py
+++ analysis_parameters.py
@@ -25,7 +25,15 @@ down_fs = int(signal_sample_frequency / deconv_sample_frequency)
 linestylemap = {'PP': [[.5,.5,.5],'-',None,None,None],
 				'UP': ['m','-','o','m','w'],
 				'PU': ['y','-','o','y','w'],
-				'UU': ['c','--','o','c','c']}
+				'UU': ['c','--','o','c','c'],
+				'col_PP': [[.5,.5,.5],'-',None,None,None],
+				'col_UP': ['m','-','o','m','w'],
+				'col_PU': ['y','-','o','y','w'],
+				'col_UU': ['c','--','o','c','c'],
+				'ori_PP': [[.5,.5,.5],'-',None,None,None],
+				'ori_UP': ['m','-','o','m','w'],
+				'ori_PU': ['y','-','o','y','w'],
+				'ori_UU': ['c','--','o','c','c']}
 
 
 sublist = ['AB','AC','AF','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AS','AT','AV','AX','AZ','BA','BB','BC','IAA','IAC','IAF','IAH','IAJ','IAK','IAL','IAM','IAN','IAO','IAP','IAQ']
