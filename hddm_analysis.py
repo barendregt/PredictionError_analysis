9ac85908eb6ee742d006905cbc74a83643ab2405
Martijn Barendregt <barendregt@fastmail.com>
2018-01-16 15:44:07 +0100

    update

--- hddm_analysis.py
+++ hddm_analysis.py
@@ -7,8 +7,8 @@ matplotlib.use('WebAgg')
 # import matplotlib.pyplot as plt
 # import seaborn as sn
 
-# from joblib import Parallel, delayed
-# import multiprocessing
+from joblib import Parallel, delayed
+import multiprocessing
 
 from math import *
 import os,glob,sys,platform
@@ -28,20 +28,41 @@ new_fit = False
 data = hddm.load_csv('%s.csv'%filename)
 
 
+use_data = data[(data['rt'] > 0) & (data['rt'] < 4)]
+
+use_data = hddm.utils.flip_errors(use_data)
+
 # fit initial model
-m = hddm.HDDM(data[data['missed_response']==0], depends_on = {'v':['TR_PE','TI_PE'],'t':['TR_PE','TI_PE']})
 
 
-embed()
+
+# embed()
 
 
 # m = hddm.HDDM(data[data['missed_response']==0], depends_on = {'v':['TR_PE','TI_PE']})
 
-# m.find_starting_values()
-
-# runii = 1
-
-# m.sample(500, burn = 250, dbname = '%s-%i-db.pickle'%(filename,runii), db = 'pickle')
 
 
+models = []
+
+# for runii in range(5):
+
+def run_hddm(runii):
+
+	m = hddm.HDDM(use_data, p_outlier = 0.05, depends_on = {'v':['TR_PE','TI_PE'],'t':['TR_PE','TI_PE']})
+
+	m.find_starting_values()
+
+	m.sample(5000, burn = 2500, dbname = '%s-%i-db.pickle'%(filename,runii), db = 'pickle')
+
+	m.save('model-run%i'%runii)
+
+	# models.append(m)
+
+# Run everything in parallel for speed 
+num_jobs = 5
+
+Parallel(n_jobs=num_jobs)(delayed(run_hddm)(runii) for runii in range(num_jobs))	
+
+# hddm.analyze.gelman_rubin(models)
 # embed()
